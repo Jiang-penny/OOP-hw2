4daf19fbe90f8e81eac43ba3db5d83760b7d872a
PennyJiang <PennyJiang@asus>
2020-03-20 20:02:48 +0800

    second commit

--- src/lab3/Hello.java
+++ src/lab3/Hello.java
@@ -2,9 +2,9 @@ package lab3;
 
 public class Hello {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+  public static void main(String[] args) {
+	// TODO Auto-generated method stub
+    System.out.println("Hello");  
 	}
 
 }
